f65ca72d7d9386b913b6870f6ae1be211a0531fd
lryain <47129927@qq.com>
2025-09-08 18:14:50 +0800

    fix compile bug

--- wm8960.c
+++ wm8960.c
@@ -1441,7 +1441,7 @@ static void wm8960_set_pdata_from_of(struct i2c_client *i2c,
 }
 
 // static int wm8960_i2c_probe(struct i2c_client *i2c)
-static int wm8960_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
+static int wm8960_i2c_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 {
 	struct wm8960_data *pdata = dev_get_platdata(&i2c->dev);
 	struct wm8960_priv *wm8960;
